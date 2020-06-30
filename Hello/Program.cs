ae3f5344c5e720e0cb7b8f2e2e5dee732262ce04
cichali <cichyakms@gmail.com>
2020-06-30 19:15:28 +0200

    Revert "Projekt dzienniczka ucznia"
    This reverts commit e4a59c30b1335b5300e23c9898f40fd286951dcf.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,55 +10,9 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            for (; ;) //nieskończona pętla
-            {
-                Greeting();
-                CheckAge();
-                Settings();
-            }
-        }
-
-        ///<summary>
-        ///ustawienia konsoli
-        ///</summary>
-        private static void Settings()
-        {
+            Console.WriteLine("Witaj");
             Console.ReadKey(); //oczekuje na wciśnięcie klawisza
-            Console.Clear();
-            Console.ResetColor();
-        }
-        ///<summary>
-        ///sprawdzanie wieku użytkownika
-        ///</summary>
-        private static void CheckAge()
-        {
-            Console.Write("Podaj swój wiek: ");
-            int age;
-            bool result = int.TryParse(Console.ReadLine(), out age); //zamiana string na int
-
-            if (age >= 18)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Jesteś pełnoletni");
-            }
-            else if (result == false) // sprawdzenie czy wprowadzony wiek jest poprawny
-            {
-                Console.WriteLine("Źle wpisano wiek");
-            }
-            else
-            {
-                Console.WriteLine("Nie jesteś pełnoletni");
-            }
-        }
-        ///<summary>
-        ///witanie użytkownika
-        ///</summary>
-        private static void Greeting()
-        {
-            Console.Write("Wpisz swoje imię: ");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Witaj " + name);
+            
         }
     }
 }
