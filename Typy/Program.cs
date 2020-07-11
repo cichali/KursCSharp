629a4bfc704b71107f2e553d9b3586256531e1c8
cichali <cichyakms@gmail.com>
2020-07-11 22:08:18 +0200

    Ref, out, immutable, struktury, enum

--- Typy/Program.cs
+++ Typy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,22 +9,48 @@ namespace Typy
 {
     class Program
     {
+        static void GiveName(ref Diary diary)
+        {
+            diary = new Diary();
+            diary.Name = "Nowy dzienniczek Marcina";
+        }
+
+        static void IncrementNumber(ref int number)
+        {
+            number = 42;
+        }
         static void Main(string[] args)
+        {
+            //PassByValueAndRef();
+
+            Immutable();
+        }
+
+        private static void Immutable()
+        {
+            string name = " Marcin ";
+            name = name.Trim();
+
+            DateTime date = new DateTime(2015, 3, 10);
+            date = date.AddHours(48);
+
+            Console.WriteLine(name);
+            Console.WriteLine(date);
+        }
+
+        private static void PassByValueAndRef()
         {
             Diary d1 = new Diary();
             Diary d2 = d1;
 
-            d1 = new Diary();
-
-            d1.Name = "Dzienniczek Emilki";
+            GiveName(ref d2);
 
             Console.WriteLine(d2.Name);
 
             int x1 = 4;
-            int x2 = x1;
-            x1 = 100;
-            Console.WriteLine(x2);
+            IncrementNumber(ref x1);
 
+            Console.WriteLine(x1);
         }
     }
 }
